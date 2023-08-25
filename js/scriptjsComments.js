2905590d267f001b597911dee994ac34e7a31664
biba25 <biba25230@gmail.com>
2023-08-25 03:05:05 +0300

    commit_012

--- js/scriptjsComments.js
+++ js/scriptjsComments.js
@@ -1,48 +1,54 @@
-let comments = [];
+let comments = []; //
 loadComments();
 
 document.getElementById("comment-add").onclick = function () {
-  let commentName = document.getElementById("comment-name");
+  //добаляем событие на кнопку, достаем комментарии из формы
+  let commentName = document.getElementById("comment-name"); // получаем весь обьект
   let commentBody = document.getElementById("comment-body");
 
   let comment = {
+    // получаем значение из самой формы
     name: commentName.value,
     body: commentBody.value,
-    time: Math.floor(Date.now() / 1000),
+    time: Math.floor(Date.now() / 1000), // текущее время комментария с округлением
   };
 
-  commentName.value = "";
+  commentName.value = ""; // очищаем форму
   commentBody.value = "";
 
-  comments.push(comment);
-  saveComments();
-  showComments();
+  comments.push(comment); // добавляем comment  в массив comments
+  saveComments(); // запуск функции сохранения
+  showComments(); // запуск функции отображения на странице
 };
 
 function saveComments() {
-  localStorage.setItem("comments", JSON.stringify(comments));
+  localStorage.setItem("comments", JSON.stringify(comments)); // сохраняям комментарии в localStorage
 }
-
 function loadComments() {
+  // сохраняям комментарии на странице
   if (localStorage.getItem("comments"))
     comments = JSON.parse(localStorage.getItem("comments"));
   showComments();
 }
 
 function showComments() {
-  let commentField = document.getElementById("comment-field");
+  // выводим на экран
+  let commentField = document.getElementById("comment-field"); // будет отображаться в этом поле
   let out = "";
   comments.forEach(function (item) {
+    // перебираем масив
     out += `<p class="text-right small"><em>${timeConverter(
+      // время
       item.time
     )}</em></p>`;
-    out += `<p class="alert alert-primary" role="alert">${item.name}</p>`;
-    out += `<p class="alert alert-success" role="alert">${item.body}</p>`;
+    out += `<p class="alert alert-primary" role="alert">${item.name}</p>`; // имя
+    out += `<p class="alert alert-success" role="alert">${item.body}</p>`; // комментарий
   });
   commentField.innerHTML = out;
 }
 
 function timeConverter(UNIX_timestamp) {
+  // функция даты
   var a = new Date(UNIX_timestamp * 1000);
   var months = [
     "Jan",
@@ -58,15 +64,13 @@ function timeConverter(UNIX_timestamp) {
     "Nov",
     "Dec",
   ];
-  var year = a.getFullYear();
-  var month = months[a.getMonth()];
-  var date = a.getDate();
-  var hour = a.getHours();
-  var min = a.getMinutes();
-  var sec = a.getSeconds();
-  var time =
+  let year = a.getFullYear();
+  let month = months[a.getMonth()];
+  let date = a.getDate();
+  let hour = a.getHours();
+  let min = a.getMinutes();
+  let sec = a.getSeconds();
+  let time =
     date + " " + month + " " + year + " " + hour + ":" + min + ":" + sec;
   return time;
 }
-
-
--- js/scriptjsSorting.js
+++ js/scriptjsSorting.js
@@ -1,13 +1,14 @@
-let filters = document.querySelectorAll("a[data-filter]");
+let filters = document.querySelectorAll("a[data-filter]"); // получаем ссылки с атрибктом data-filter
 
 for (let filter of filters) {
   filter.addEventListener("click", function (e) {
-    e.preventDefault();
+    e.preventDefault(); // изменяет значение по умолчанию у ссылок
 
-    let catId = filter.getAttribute("data-filter");
+    let catId = filter.getAttribute("data-filter"); // получаем значение атрибута data-filter
     let workCat = document.querySelectorAll(".portfolio__col");
 
     workCat.forEach(function (c) {
+      // функция с добавлением и удалением класса
       if (catId === "all") {
         c.classList.remove("hide");
       } else {
